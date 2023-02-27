3ba97b8d97a4f82ed232d25484dfc72f84488748
dhtjddls <dhtjddls12@naver.com>
2023-02-27 19:16:10 +0900

    feat: redux 적용, proxy middle ware 적용, cors 설정

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
-const config = require("./server/comfig/key");
+const config = require("./config/key");
 const { User } = require("./models/User");
 const cookieParser = require("cookie-parser");
 const { auth } = require("./middleware/auth");
@@ -72,6 +72,10 @@ app.get("/api/users/logout", auth, (req, res) => {
       success: true,
     });
   });
+});
+
+app.get("/api/hello", (req, res) => {
+  res.send("hi");
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
