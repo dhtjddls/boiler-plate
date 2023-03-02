1fdc0a7148c12ccf4359b7446d18195c94771515
dhtjddls <dhtjddls12@naver.com>
2023-03-02 23:51:07 +0900

    feat: 회원가입 기능 완성!

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("hello world"));
-app.post("api/users/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
